586f1cf9e1ae7d2e67e1bde7c8fa8ed535827824
Niccolo Brogi <nkkollaw@gmail.com>
2019-07-01 18:03:33 +0200

    Upgrade MIUR data

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('VAFFASCHOOL_SQLITE_FILE', __DIR__ . '/data/schools.sqlite');
 
-define('VAFFASCHOOL_RAW_DATA_DIR', __DIR__ . '/data/raw/MIUR/2018');
+define('VAFFASCHOOL_RAW_DATA_DIR', __DIR__ . '/data/raw/MIUR/2019');
 define('VAFFASCHOOL_RAW_DATA_FILE_TYPES', 'json'); // extensions of files that we want to process, separated by comma for multiple file types
